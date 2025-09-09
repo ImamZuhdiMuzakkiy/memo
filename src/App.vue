725e211df9b6cf025ad46a3c92a1c7365c4040a7
Imam <imamzuhdimuzakkiy1@gmail.com>
2025-09-09 10:43:28 +0700

    feat: initialize Vue.js sticky note application with Vite
    - Add package.json to manage dependencies and scripts
    - Include favicon.ico for application branding
    - Create App.vue as the main component with a basic layout
    - Set up main.js to bootstrap the Vue application
    - Configure Vite with vue and vue-devtools plugins

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1 class="title">Memo</h1>
+        <button>+</button>
+      </header>
+      <div>[card]</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .container {
+    max-width: 900px;
+    padding: 10px;
+    margin: 0 auto;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
